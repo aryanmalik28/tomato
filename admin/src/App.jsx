793ac8311b4c9b82f5a3ca71a7edaca343b8461a
Aryan Malik <149329741+aryanmalik28@users.noreply.github.com>
2024-10-30 04:26:29 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './components/pages/Orders/Orders.jsx'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url= "http://localhost:4000"
+  const url= "https://tomato-backend-2ral.onrender.com"
 
   return (
     <div>
